52adb2360a37a1bf238dd7d83f100e8cccede624
adianbu <57458067+adianbu@users.noreply.github.com>
2021-06-13 01:42:09 +0530

    No internet connection case handled

--- app/src/main/java/com/example/earthquake/EarthquakeActivity.java
+++ app/src/main/java/com/example/earthquake/EarthquakeActivity.java
@@ -2,9 +2,12 @@ package com.example.earthquake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -90,12 +93,32 @@ public class EarthquakeActivity extends AppCompatActivity  implements LoaderMana
 //        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
 //        task.execute(USGS_REQUEST_URL);
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        android.app.LoaderManager loaderManager = getLoaderManager();
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.progressBar);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+
+        }
+
     }
 
 //    /**
